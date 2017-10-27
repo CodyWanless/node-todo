7cbe3ce8bdf2776b16d8e4c0895215cd351113f2
Cody Wanless <codywanless@Codys-MacBook-Pro.local>
2017-10-27 14:44:07 -0500

    Add post /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -4,7 +4,6 @@ const jwt = require('jsonwebtoken');
 const _ = require('lodash');
 const bcrypt = require('bcryptjs');
 
-
 var UserSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -63,6 +62,24 @@ UserSchema.methods.generateAuthToken = function () {
 
     return this.save().then(() => {
         return token;
+    });
+};
+
+UserSchema.statics.findByCredentials = function (email, password) {
+    return this.findOne({ email }).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    resolve(user);
+                } else {
+                    reject();
+                }
+            });
+        });
     });
 };
 
